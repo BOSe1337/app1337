ae598213bed35553adf1d44e7d1227eb9800f59e
bobrik <1337@ya.ru>
2023-07-18 21:41:53 +0700

    push app1337

--- api_logs/urls.py
+++ api_logs/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.static import static
 from django.urls import path
 
 from api_logs.views import UserAccessView
+from app1337 import settings
 
 urlpatterns = [
     path("",UserAccessView.as_view()), #имортируем UserAccessView
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app1337/settings.py
+++ app1337/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / '/static/'  # Указываем путь до папки статики и ctrl+L чтобы выровнять текст до pep8
+STATIC_ROOT = BASE_DIR / 'static'  # Указываем путь до папки статики и ctrl+L чтобы выровнять текст до pep8
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
