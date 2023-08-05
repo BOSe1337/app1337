66c525b8e81213548780c497aff235bae51be2ca
bobrik <1337@ya.ru>
2023-08-05 18:32:35 +0700

    add admin

--- api_logs/admin.py
+++ api_logs/admin.py
@@ -4,16 +4,28 @@ from .models import PermittedUser, AlienUser
 
 @admin.register(PermittedUser)
 class PermittedUserAdmin(admin.ModelAdmin):
-    list_display = ("time_login",)
+    list_display = (
+        "username", "type_of_log", "userdomain", "hostname", "ipaddress", "type_of_service", "time_login",
+        "localdatetime")
     fields = (
-    "username", "type_of_log", "userdomain", "hostname", "ipaddress", "type_of_service", "time_login", "localdatetime")
+        "username", "type_of_log", "userdomain", "hostname", "ipaddress", "type_of_service", "time_login",
+        "localdatetime")
     readonly_fields = ("time_login",)
-
+    list_filter = (
+        "username", "type_of_log", "userdomain", "hostname", "ipaddress", "type_of_service", "time_login",
+        "localdatetime")
+    search_fields = ("username", "type_of_log", "userdomain", "hostname", "ipaddress", "type_of_service", "time_login",
+        "localdatetime")
 
 @admin.register(AlienUser)
 class AlienUserAdmin(admin.ModelAdmin):
-    list_display = ("time_login",)
+    list_display = ("username", "type_of_log", "userdomain", "hostname", "ipaddress", "type_of_service", "time_login",
+                    "localdatetime", "session_ip")
     fields = (
         "username", "type_of_log", "userdomain", "hostname", "ipaddress", "type_of_service", "time_login",
         "localdatetime", "session_ip")
     readonly_fields = ("time_login",)
+    list_filter = ("username", "type_of_log", "userdomain", "hostname", "ipaddress", "type_of_service", "time_login",
+                   "localdatetime", "session_ip")
+    search_fields = ("username", "type_of_log", "userdomain", "hostname", "ipaddress", "type_of_service", "time_login",
+        "localdatetime", "session_ip")
