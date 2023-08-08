4ad8a14753051efda5ed4684394778cdea6137da
bobrik <1337@ya.ru>
2023-08-08 20:03:49 +0700

    add stateuser

--- api_logs/models.py
+++ api_logs/models.py
@@ -11,3 +11,16 @@ class PermittedUser(models.Model):
     time_login = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания записи')
     localdatetime = models.DateTimeField(verbose_name='localdatetime')
     session_ip = models.CharField(max_length=255, verbose_name="IP машины с которой пришёл запрос")
+
+
+class StateUser(models.Model):
+    username = models.CharField(max_length=255, verbose_name="username")
+    type_of_log = models.CharField(max_length=255, verbose_name="logon/logoff")
+    userdomain = models.CharField(max_length=255, verbose_name="userdomain")
+    hostname = models.CharField(max_length=255, verbose_name="hostname")
+    ipaddress = models.CharField(max_length=255, verbose_name="ip address")
+    type_of_service = models.CharField(max_length=255, verbose_name="Тип Входа в систему")
+    time_login = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания записи')
+    localdatetime = models.DateTimeField(verbose_name='localdatetime')
+    session_ip = models.CharField(max_length=255, verbose_name="IP машины с которой пришёл запрос")
+    state = models.CharField(max_length=255, verbose_name="Текущее состояние")
