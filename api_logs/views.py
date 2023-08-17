ad7b0f2b6c08a26e979b75f3aec730237457aa9a
bobrik <1337@ya.ru>
2023-08-17 23:24:51 +0700

    add dinamic localdatetime

--- api_logs/views.py
+++ api_logs/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import threading
 
+import pytz
 from rest_framework.response import Response  # Импорты классов
 from rest_framework.views import APIView
 
@@ -38,12 +39,20 @@ class UserAccessView(APIView):  # класс который работает с 
                 state="active"
             )
 
+    @staticmethod
+    def change_user(user, **kwargs):#Добавил ф-цию которая меняет значения для пользователя валуе
+        for key, value in kwargs.items():
+            setattr(user, key, value)
+        user.save()
+
     def post(self, request, *args, **kwargs):  # ф-ция Post обрабатывает все входящие post запросы
         client_ip = self.get_client_ip(request)
         type_request = request.data['type'].split()[-1]
         # Получаем тип входа из запроса. Разбиваем по пробелам и берем последний элемент
         # (потому что в последнем элементе лежит ключевое слово)
         if UserAccessView.lock.acquire(blocking=False):
+            localdatetime = pytz.timezone('Europe/Moscow').localize(
+                datetime.strptime(request.data["localdatetime"], "%Y-%m-%d %H:%M:%S"))
             try:
                 PermittedUser.objects.create(  # создаём запись в таблице PermittedUser
                     username=request.data["username"],
@@ -52,45 +61,40 @@ class UserAccessView(APIView):  # класс который работает с 
                     hostname=request.data["hostname"],
                     ipaddress=request.data["ipaddress"],
                     type_of_service=request.data["logontype"],
-                    localdatetime=datetime.strptime(request.data["localdatetime"], "%Y-%m-%d %H:%M:%S"),
+                    localdatetime=localdatetime,
                     session_ip=client_ip,
                 )
-                if type_request == 'logon':
-                    self.state_user(request, client_ip)
+                if type_request in ['(LOGON)', '(UNLOCK)', '(RECONNECT)']: #укоротил условие
+                    self.change_user(
+                        self.state_user(request, client_ip),
+                        state="active",
+                        localdatetime=localdatetime
+                    )
                     # Либо получаем такого пользователя, либо создаём
-                elif type_request == 'logoff':
+                elif type_request == '(LOGOFF)':
                     self.state_user(request, client_ip).delete()
                     # Получаем такого пользователя и удаляем
-                elif type_request == 'lock':
-                    user = self.state_user(request, client_ip)
-                    user.state = "locked"
-                    user.save()
-                    # Получаем такого пользователя и меняем state на locked
-                elif type_request == 'unlock':
-                    user = self.state_user(request, client_ip)
-                    user.state = "active"
-                    user.save()
-                    # Получаем такого пользователя и меняем state на active
-                elif type_request == 'disconnect':
-                    user = self.state_user(request, client_ip)
-                    user.state = "disconnect"
-                    user.save()
-                    # Получаем такого пользователя и меняем state на disconnect
-                elif type_request == 'reconnect':
-                    user = self.state_user(request, client_ip)
-                    user.state = "active"
-                    user.save()
-                    # Получаем такого пользователя и меняем state на active
-                elif type_request == 'powerOFF':
-                    users = StateUser.objects.filter(
-                        hostname=request.data["hostname"],
-                        localdatetime__lte=datetime.strptime(request.data["localdatetime"], "%Y-%m-%d %H:%M:%S")
+                elif type_request == '(LOCK)':
+                    self.change_user(
+                        self.state_user(request, client_ip),
+                        state="locked",
+                        localdatetime=localdatetime
                     )
-                    for user in users:
-                        user.delete()
+                    # Получаем такого пользователя и меняем state на locked
+                elif type_request == '(DISCONNECT)':
+                    self.change_user(
+                        self.state_user(request, client_ip),
+                        state="disconnect",
+                        localdatetime=localdatetime
+                    )
+                    # Получаем такого пользователя и меняем state на disconnect
+                elif type_request == '(POWEROFF)':
+                    StateUser.objects.filter(
+                        hostname=request.data["hostname"],
+                        localdatetime__lte=localdatetime#lte меньше или равно
+                    ).delete()
                     # Получаем всех пользователей по hostname
                     # localdatetime__lte (время меньше или равно) localdatetime события выключения и удаляем такие записи
             finally:
                 UserAccessView.lock.release()
-
             return Response("Insert done")
